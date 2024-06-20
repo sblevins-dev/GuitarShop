d0283292611296ce5ce3829db8dd37f05829b490
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-06-20 15:54:10 -0400

    added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='min-h-screen  py-20 p-28 bg-white text-black flex flex-col gap-10'>
+      <h1 className='text-4xl font-bold'>
+        About
+      </h1>
+    </div>
+  )
+}
